95c1f7702a09905b235588f3632f5d93fe690617
Loser1025 <drib189@gmail.com>
2026-06-22 10:40:56 +0900

    fix(wuhu-generals-ranking): adjust portrait image crop offset to 12.5%

--- wuhu-generals-ranking/src/components/GeneralCard.tsx
+++ wuhu-generals-ranking/src/components/GeneralCard.tsx
@@ -76,7 +76,7 @@ export default function GeneralCard({ general, index, maxStats }: { general: Gen
               setImgLoaded(true);
               const img = e.currentTarget;
               if (img.naturalHeight > img.naturalWidth) {
-                setObjectPosition('center 25%');
+                setObjectPosition('center 12.5%');
               }
             }}
             onError={() => setImgError(true)}
