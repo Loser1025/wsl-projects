c52a2dc784e38d738b030bdeb996660cf2e9d3f9
Loser1025 <drib189@gmail.com>
2026-06-22 11:30:10 +0900

    fix(wuhu-generals-ranking): move portrait crop offset 5pts closer to center

--- wuhu-generals-ranking/src/components/GeneralCard.tsx
+++ wuhu-generals-ranking/src/components/GeneralCard.tsx
@@ -76,7 +76,7 @@ export default function GeneralCard({ general, index, maxStats }: { general: Gen
               setImgLoaded(true);
               const img = e.currentTarget;
               if (img.naturalHeight > img.naturalWidth) {
-                setObjectPosition('center 35%');
+                setObjectPosition('center 40%');
               }
             }}
             onError={() => setImgError(true)}
